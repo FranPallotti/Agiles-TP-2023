6d8f2170dcaa02078b6750746055a3432cd416a1
qxtclxr <josefloresicr@gmail.com>
2024-05-30 12:21:59 -0300

    Se agrega plugin "maven-shade" y clase "Main".

---
+++ src/main/java/isi/agiles/Main.java
@@ -0,0 +1,14 @@
+package isi.agiles;
+
+/*
+ * Esta clase es importante para empaquetar la app en un ejecutable .jar.
+ * Va a ser la clase principal del sistema, para correr la aplicacion:
+ *      mvn clean javafx:run
+ * Y se va a ejecutar esta clase.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
