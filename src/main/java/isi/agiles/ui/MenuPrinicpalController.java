b1fe11541971f3848a677edc4051a0b092334b53
Patokka <patogaggiotti@gmail.com>
2024-05-30 19:01:17 -0300

    Agregado de menu principal, ver compilacion

---
+++ src/main/java/isi/agiles/ui/MenuPrinicpalController.java
@@ -0,0 +1,54 @@
+package isi.agiles.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+
+public class MenuPrinicpalController {
+    @FXML
+    private Button botonDarAltaTitular;
+
+    @FXML
+    private Button botonDarAltaUsuario;
+
+    @FXML
+    private Button botonEmitirLicencia;
+
+    @FXML
+    private Button botonImprimirLicencia;
+
+    @FXML
+    private Button botonListarLicenciasExpiradas;
+
+    @FXML
+    private Button botonListarLicenciasVigentes;
+
+    @FXML
+    private Button botonModificarCostorsLicencias;
+
+    @FXML
+    private Button botonModificarDatosTitular;
+
+    @FXML
+    private Button botonModificarDatosUsuario;
+
+    @FXML
+    private Button botonRenovarLicencia;
+
+    @FXML
+    private AnchorPane frameMenuPrincipal;
+
+    @FXML
+    private Label labelMenuPrincipal;
+
+    @FXML
+    private ImageView logoStaFeDer;
+
+    @FXML
+    private ImageView logoStaFeIzq;
+
+}
+
