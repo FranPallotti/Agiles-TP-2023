0fea681bd6ece6e9f78a171acfda8d230a4266ae
Patokka <patogaggiotti@gmail.com>
2024-06-28 15:50:21 -0300

    a

--- src/main/java/isi/agiles/logica/GestorLicencia.java
+++ src/main/java/isi/agiles/logica/GestorLicencia.java
@@ -2,6 +2,7 @@ package isi.agiles.logica;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
